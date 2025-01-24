373669d1d9de3e81106256d1e5cf24ea897c16c0
Marcus <interose@gmail.com>
2025-01-24 13:02:28 +0100

    added frontend methods for updating values

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Gas\Daily as GasDaily;
+use App\Entity\Gas\Hourly as GasHourly;
+use App\Entity\Photovoltaics\Daily as PvDaily;
+use App\Entity\Photovoltaics\Hourly as PvHourly;
+use App\Entity\Power\Daily as PowerDaily;
+use App\Entity\Power\Hourly as PowerHourly;
+use App\Entity\Sensor;
+use App\Entity\Solar\Daily as SolarDaily;
+use App\Entity\Solar\Hourly as SolarHourly;
+use App\Lib\SML_PARSER;
+use App\Lib\SmlParser;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api')]
+class ApiController extends AbstractController
+{
+    #[Route('/gas/add', name: 'gas_add', methods: ['POST'])]
+    public function gasAddAction(LoggerInterface $logger, EntityManagerInterface $em): Response
+    {
+        try {
+            $em->getRepository(GasDaily::class)->update();
+            $em->getRepository(GasHourly::class)->update();
+        } catch (\Exception $e) {
+            $logger->error($e->getMessage());
+
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    #[Route('/power/add', name: 'power_add', methods: ['POST'])]
+    public function powerAddAction(
+        Request $request,
+        LoggerInterface $logger,
+        EntityManagerInterface $em,
+        SmlParser $parser,
+    ): Response {
+        try {
+            $record = $parser->parse_sml_hexdata($request->getContent(), 'CRC16_X_25');
+            $values = $record['body']['vallist'];
+
+            foreach ($values as $value) {
+                $obis = $value['OBIS'] ?? '';
+
+                if ('1-0:1.8.0*255' === $obis) {
+                    $em->getRepository(PowerDaily::class)->update($value['value'], (int) log10($value['scaler']));
+                } elseif ('1-0:16.7.0*255' === $obis) {
+                    $em->getRepository(PowerHourly::class)->update($value['value'], (int) log10($value['scaler']));
+                }
+            }
+        } catch (\Exception $e) {
+            $logger->error($e->getMessage());
+
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    #[Route('/pv/add', name: 'pv_add', methods: ['POST'])]
+    public function photovoltaicsAddAction(
+        Request $request,
+        LoggerInterface $logger,
+        EntityManagerInterface $em
+    ): Response {
+        try {
+            $content = json_decode($request->getContent(), true);
+            if (false === $content || is_null($content)) {
+                throw new \Exception('Could not decode content!');
+            }
+
+            $current = $content['switch:0']['apower'] ?? 0;
+            $total = $content['switch:0']['aenergy']['total'] ?? 0;
+
+            // format the numbers
+            $current = round($current * 100);
+            $current = (int) $current;
+
+            $total = round($total);
+            $total = (int) $total;
+
+            $em->getRepository(PvDaily::class)->update($total);
+            $em->getRepository(PvHourly::class)->update($current);
+        } catch (\Exception $e) {
+            $logger->error($e->getMessage());
+
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    #[Route('/solar/add', name: 'solar_add', methods: ['POST'])]
+    public function solarAddAction(Request $request, LoggerInterface $logger, EntityManagerInterface $em): Response
+    {
+        try {
+            $content = json_decode($request->getContent(), true);
+            if (false === $content || is_null($content) || !array_key_exists('value', $content)) {
+                throw new \Exception('Could not decode content!');
+            }
+
+            $yieldTodayTotal = intval($content['value']);
+
+            $em->getRepository(SolarDaily::class)->update($yieldTodayTotal);
+            $em->getRepository(SolarHourly::class)->update($yieldTodayTotal);
+        } catch (\Exception $e) {
+            $logger->error($e->getMessage());
+
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null, Response::HTTP_OK);
+    }
+
+    #[Route('/sensor/add', name: 'sensor_add', methods: ['POST'])]
+    public function sensorAddAction(Request $request, LoggerInterface $logger, EntityManagerInterface $em): Response
+    {
+        try {
+            $content = json_decode($request->getContent(), true);
+            if (false === $content || is_null($content) || !array_key_exists('m', $content)) {
+                throw new \Exception('Could not decode content!');
+            }
+
+            $mac = $content['m'];
+            unset($content['m']);
+
+            $em->getRepository(Sensor::class)->update($mac, $content);
+        } catch (\Exception $e) {
+            $logger->error($e->getMessage());
+
+            return new Response(null, Response::HTTP_BAD_REQUEST);
+        }
+
+        return new Response(null, Response::HTTP_OK);
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,3 +9,33 @@ if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
 } elseif (method_exists(Dotenv::class, 'bootEnv')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
 }
+
+$env = 'test';
+
+passthru(
+    sprintf(
+        'php bin/console doctrine:database:drop --if-exists  --force --env=%s -q',
+        $env
+    )
+);
+
+passthru(
+    sprintf(
+        'php bin/console doctrine:database:create --env=%s -q',
+        $env
+    )
+);
+
+passthru(
+    sprintf(
+        'php bin/console doctrine:schema:create --env=%s -q',
+        $env
+    )
+);
+
+passthru(
+    sprintf(
+        'php bin/console doctrine:fixtures:load --env=%s -q',
+        $env
+    )
+);
