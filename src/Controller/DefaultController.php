2d4f6bc453e50ce30d95ba8f6e9d3043ffc08358
Marcus Meirose <interose@gmail.com>
2025-02-02 16:17:08 +0100

    enable security for default controller

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,7 +7,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app_dashboard')]
